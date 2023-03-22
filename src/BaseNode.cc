d93f709bc97ecd3ad3b9c4f5b22f2c28e4653d3a
Chandler Scott <scottcd1@etsu.edu>
2023-03-21 22:29:35 -0400

    more worky work

--- src/BaseNode.cc
+++ src/BaseNode.cc
@@ -14,15 +14,23 @@ class BaseNode : public cSimpleModule
 // The module class needs to be registered with OMNeT++
 Define_Module(BaseNode);
 
+/*
+    Initialize the base node. If node's name is ding, send greeting message.
+*/
 void BaseNode::initialize()
 {
+    // if I am lead ding, send greeting message
     if (strcmp("ding", getName()) == 0) {
         send(new cMessage("Greetings"), "out");
     }
 }
 
+/*
+    Event when node receives a message. If ding, return 'Howdy', else, return 'Hey'.
+*/
 void BaseNode::handleMessage(cMessage *msg)
 {
+    // receive, digest, then delete message
     EV << "Received message: " << msg->getFullName() << omnetpp::endl;
     delete msg;
 
