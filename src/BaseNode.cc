a117c14b4b12bdc3c8df2728b8ddd0d954b764db
Chandler Scott <scottcd1@etsu.edu>
2023-03-23 23:15:34 -0400

    did some work

--- src/BaseNode.cc
+++ src/BaseNode.cc
@@ -5,10 +5,15 @@ using namespace omnetpp;
 
 class BaseNode : public cSimpleModule
 {
+  private:
+    long receivedMessages;
+    long sentMessages;
+    bool initializeComs;
+  
   protected:
-    // The following redefined virtual function holds the algorithm.
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
+    virtual void finish() override;
 };
 
 // The module class needs to be registered with OMNeT++
@@ -19,9 +24,16 @@ Define_Module(BaseNode);
 */
 void BaseNode::initialize()
 {
-    // if I am lead ding, send greeting message
-    if (strcmp("ding", getName()) == 0) {
+    initializeComs = par("initiate");
+    receivedMessages = 0;
+    sentMessages = 0;
+
+    WATCH(receivedMessages);
+    WATCH(sentMessages);
+
+    if (initializeComs == true) {
         send(new cMessage("Greetings"), "out");
+        sentMessages++;
     }
 }
 
@@ -32,13 +44,27 @@ void BaseNode::handleMessage(cMessage *msg)
 {
     // receive, digest, then delete message
     EV << "Received message: " << msg->getFullName() << omnetpp::endl;
+    receivedMessages++;
     delete msg;
 
     // return a message
-    if (strcmp("ding", getName()) == 0) {
+    if (initializeComs == true) {
         send(new cMessage("Howdy"), "out");
+        sentMessages++;
     }
     else {
         send(new cMessage("Hey"), "out");
+        sentMessages++;
     }
+}
+
+/*
+    Record statistics gathered in data members at the end of simulation.
+    Only runs if the simulation finishes successfully.
+*/
+void BaseNode::finish()
+{
+    EV << "Some cool data for " << getFullName() << omnetpp::endl;
+    EV << "Number of Messages Sent:     " << sentMessages << omnetpp::endl;
+    EV << "Number of Messages Received: " << receivedMessages << omnetpp::endl;
 }
